33fc5955ea3eb37aa9d9aedbef2ca10a2699b052
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-11 19:58:46 -0500

    Building Streamlit App

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import os
+
+dataExploration = st.container()
+
+record_data = pd.read_csv('current_usapl_american_raw_records.csv')
+
+with dataExploration:
+    st.title('Natural Strength Building')
+    st.subheader('Progress With Real Raw Data')
+    st.header('Dataset: American USAPL Raw Powerlifting Records')
+    st.markdown('I scraped this dataset from... https://usapl.liftingdatabase.com/')
+    st.markdown('**It contains the current Male and Female American Raw Powerlifting Records recorded by USAPL**')
+    st.text('Below is the DataFrame')
+    st.write(record_data)
