63dd7a5a27b60316846ec8c7510cdcb3d890c109
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-12 14:30:19 -0500

    Displaying charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
 
+'''
+GLOBAL VARIABLES
+'''
+# Units
+metric_units = False
+
+# Sex
+male = True
 
 def lbs_to_kg(lbs):
     return lbs * 0.453592
@@ -9,6 +18,24 @@ def lbs_to_kg(lbs):
 
 def kg_to_lbs(kg):
     return kg * 2.20462
+
+
+def compute_weight_class(weight: float):
+    if not metric_units:
+        weight = lbs_to_kg(weight)
+    if male:
+        classes = [52.0, 56.0, 60.0, 67.5, 75.0, 82.5, 90.0, 100.0, 110.0, 125.0, 140.0, 141.0]
+    else:
+        classes = [44.0, 48.0, 52.0, 56.0, 60.0, 67.5, 75.0, 82.5, 90.0, 100.0, 101.0]
+
+    for _class in classes:
+        if weight <= _class:
+            return _class
+    # If not in previous classes, return max weight class
+    return classes[-1]
+
+
+
 
 @st.cache
 def load_record_data():
@@ -82,12 +109,22 @@ with dataQuerying:
 
     st.write(query_args(wc_input, lift_input, sex_input))
 
-side_bar = st.sidebar.selectbox(
-    'What is my weight class?',
-    ('', "Home phone", "Mobile phone")
-)
+units = st.sidebar.radio('Units', ['Lbs', 'Kg'])
+# Toggle global variable
+if units == 'Lbs':
+    metric_units = False
+else:
+    metric_units = True
+
+# st.sidebar.write(units)
+# st.sidebar.write(metric_units)
+
+weight_input = st.sidebar.number_input(
+    'Let\'s figure out your weight class', min_value=0, max_value=1500)
+st.sidebar.write(f'Your weight class is {compute_weight_class(weight_input)}')
 
 # # adding a text input widget to the sidebar
 #
 # add_textbox = st.sidebar.text_input('Enter your name: ')
 
+st.bar_chart(data=record_data[['Weight (kg)', 'Weight Class']])
