81026ad9b92d88ed1be5eede0ff78048414c64de
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-12 10:29:09 -0500

    Created sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import random
 
-dataExploration = st.container()
-
-record_data = pd.read_csv('current_usapl_american_raw_records.csv')
-
-with dataExploration:
-    st.title('Natural Strength Building')
-    st.subheader('Progress With Real Raw Data')
-    st.header('Dataset: American USAPL Raw Powerlifting Records')
-    st.markdown('I scraped this dataset from... https://usapl.liftingdatabase.com/')
-    st.markdown('**It contains the current Male and Female American Raw Powerlifting Records recorded by USAPL**')
-    st.text('Below is the DataFrame')
-    st.write(record_data)
-
-# @st.cache
-def make_select_box(category: str):
+def generate_options(category: str):
     options = np.sort(record_data[category].unique())
     options = list(options)
     options.insert(0, '<select>')
@@ -34,28 +19,59 @@ def query_args(*queries):
             df = df[df[query[0]] == query[1]]
     return df
 
+dataExploration = st.container()
 
-wc_category = 'Weight Class'
-wc_options = make_select_box(wc_category)
-wc = st.selectbox(wc_category, wc_options)
-wc_input = (wc_category, wc)
+record_data = pd.read_csv('current_usapl_american_raw_records.csv')
 
-st.write('You selected:', wc)
+with dataExploration:
+    st.title('Natural Strength Building')
+    st.subheader('Progress With Real Raw Data')
+    st.header('Dataset: American USAPL Raw Powerlifting Records')
+    st.markdown('I scraped this dataset from... https://usapl.liftingdatabase.com/')
+    st.markdown('**It contains the current Male and Female American Raw Powerlifting Records recorded by USAPL**')
+    st.text('Below is the DataFrame')
+    st.write(record_data)
 
-st.write(query_args(wc_input))
-# make_select_box('Lift')
 
-# lift = st.selectbox(
-#     'Lift',
-#     np.sort(record_data['Lift'].unique()))
-#
-# st.write('You selected:', lift)
-#
-# sex = st.selectbox(
-#     'Sex',
-#     np.sort(record_data['Sex'].unique()))
-#
-# st.write('You selected:', sex)
+dataQuerying = st.container()
 
-# st.write(query_records(weight_class, lift, sex))
-query_args(wc_input)
+with dataQuerying:
+    st.subheader('Query the Records')
+    # st.header('Dataset: American USAPL Raw Powerlifting Records')
+    # st.markdown('I scraped this dataset from... https://usapl.liftingdatabase.com/')
+    # st.markdown('**It contains the current Male and Female American Raw Powerlifting Records recorded by USAPL**')
+    # st.text('Below is the DataFrame')
+    # st.write(record_data)
+
+    # Weight Class
+    wc_category = 'Weight Class'
+    wc_options = generate_options(wc_category)
+    wc = st.selectbox(wc_category, wc_options)
+    wc_input = (wc_category, wc)
+    st.write('You selected:', wc)
+
+    # Lift
+    lift_category = 'Lift'
+    lift_options = generate_options(lift_category)
+    lift = st.selectbox(lift_category, lift_options)
+    lift_input = (lift_category, lift)
+    st.write('You selected:', lift)
+
+    # Sex
+    sex_category = 'Sex'
+    sex_options = generate_options(sex_category)
+    sex = st.selectbox(sex_category, sex_options)
+    sex_input = (sex_category, sex)
+    st.write('You selected:', sex)
+
+    st.write(query_args(wc_input, lift_input, sex_input))
+
+side_bar =  st.sidebar.selectbox(
+    'What is my weight class?',
+    ('', "Home phone", "Mobile phone")
+)
+
+#adding a text input widget to the sidebar
+
+add_textbox = st.sidebar.text_input('Enter your name: ')
+
