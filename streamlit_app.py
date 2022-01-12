e0509837e4d67dc85957d751f7f300d480bb41b4
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-11 23:45:48 -0500

    Implemented select boxes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -15,12 +16,37 @@ with dataExploration:
     st.write(record_data)
 
 
-def query_records(weight_class: float, lift: str = ''):
+def query_records(weight_class: float, lift: str = '', sex: str = 'M'):
     if lift == '':
-        return record_data[record_data['Weight Class'] == weight_class]
+        return record_data[(record_data['Weight Class'] == weight_class) & (record_data['Sex'] == sex)]
     else:
-        return record_data[(record_data['Weight Class'] == weight_class) & (record_data['Lift'] == lift)]
+        return record_data[
+            (record_data['Weight Class'] == weight_class) & (record_data['Lift'] == lift) & (record_data['Sex'] == sex)]
 
 
 st.write(query_records(52.0))
 st.write(query_records(60.0, 'Raw Open - Bench press single lift'))
+
+# option = st.selectbox(
+#     'Weight Class',
+#     (52.0, 56.0, 60.0))
+
+weight_class = st.selectbox(
+    'Weight Class',
+    np.sort(record_data['Weight Class'].unique()))
+
+st.write('You selected:', weight_class)
+
+lift = st.selectbox(
+    'Lift',
+    np.sort(record_data['Lift'].unique()))
+
+st.write('You selected:', lift)
+
+sex = st.selectbox(
+    'Sex',
+    np.sort(record_data['Sex'].unique()))
+
+st.write('You selected:', sex)
+
+st.write(query_records(weight_class, lift, sex))
