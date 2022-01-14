c55e9baf8750328fa2a86d81c2afe150ffe508ed
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-14 09:37:58 -0500

    Improved records page UI with columns

--- pages/records.py
+++ pages/records.py
@@ -10,14 +10,12 @@ def app():
     def load_record_data():
         return pd.read_csv(f'data{os.path.sep}current_usapl_american_raw_records.csv')
 
+    # function to generate select box options
     def generate_options(category: str):
         options = np.sort(record_data[category].unique())
         options = list(options)
         options.insert(0, '<select>')
         return options
-        # select_box = st.selectbox(category, options, key=random.random())
-        # # user_input = ('Weight Class', weight_class)
-        # return select_box
 
     def query_args(*queries):
         df = record_data.copy()
@@ -47,31 +45,31 @@ def app():
 
     with dataQuerying:
         st.subheader('Query the Records')
-        # st.header('Dataset: American USAPL Raw Powerlifting Records')
-        # st.markdown('I scraped this dataset from... https://usapl.liftingdatabase.com/')
-        # st.markdown('**It contains the current Male and Female American Raw Powerlifting Records recorded by USAPL**')
-        # st.text('Below is the DataFrame')
-        # st.write(record_data)
 
-        # Weight Class
-        wc_category = 'Weight Class'
-        wc_options = generate_options(wc_category)
-        wc = st.selectbox(wc_category, wc_options)
-        wc_input = (wc_category, wc)
-        st.write('You selected:', wc)
+        weight_col, lift_col, sex_col = st.columns(3)
 
-        # Lift
-        lift_category = 'Lift'
-        lift_options = generate_options(lift_category)
-        lift = st.selectbox(lift_category, lift_options)
-        lift_input = (lift_category, lift)
-        st.write('You selected:', lift)
+        with weight_col:
+            # Weight Class
+            wc_category = 'Weight Class'
+            wc_options = generate_options(wc_category)
+            wc = st.selectbox(wc_category, wc_options)
+            wc_input = (wc_category, wc)
+            st.write('You selected:', wc)
 
-        # Sex
-        sex_category = 'Sex'
-        sex_options = generate_options(sex_category)
-        sex = st.selectbox(sex_category, sex_options)
-        sex_input = (sex_category, sex)
-        st.write('You selected:', sex)
+        with lift_col:
+            # Lift
+            lift_category = 'Lift'
+            lift_options = generate_options(lift_category)
+            lift = st.selectbox(lift_category, lift_options)
+            lift_input = (lift_category, lift)
+            st.write('You selected:', lift)
+
+        with sex_col:
+            # Sex
+            sex_category = 'Sex'
+            sex_options = generate_options(sex_category)
+            sex = st.selectbox(sex_category, sex_options)
+            sex_input = (sex_category, sex)
+            st.write('You selected:', sex)
 
         st.write(query_args(wc_input, lift_input, sex_input))
