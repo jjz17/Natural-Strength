6dc6f037662f91152d6bdc17176529f74ef591ab
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-16 17:56:43 -0500

    Abstracted data loading functions into utils

--- pages/utils.py
+++ pages/utils.py
@@ -1,3 +1,6 @@
+import os
+
+import pandas as pd
 import streamlit as st
 
 
@@ -11,3 +14,13 @@ def lbs_to_kg(lbs: float) -> float:
 
 def kg_to_lbs(kg: float) -> float:
     return kg * 2.20462
+
+
+@st.cache
+def load_model_training_data():
+    return pd.read_csv(f'data{os.path.sep}model_training_data.csv')
+
+
+@st.cache
+def load_record_data():
+    return pd.read_csv(f'data{os.path.sep}current_usapl_american_raw_records.csv')
