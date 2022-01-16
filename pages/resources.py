8ede856b857cab0017390da432857a0a3cb1a253
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-16 18:14:12 -0500

    Added links to relevant channels in resources

--- pages/resources.py
+++ pages/resources.py
@@ -23,4 +23,20 @@ def app():
         #             'the best choice for our users and ourselves.**')
         # st.text('Note: we are not affiliated with USAPL')
         # st.image('https://www.usapowerlifting.com/wp-content/uploads/2021/04/USA-Powerlifting-Logo.svg')
-        # st.video('https://youtu.be/zBlGzXXtQYs')
+
+        channel1, channel2, channel3 = st.columns(3)
+
+        with channel1:
+            st.subheader('Alan Thrall')
+            st.text('Powerlifter/Strongman')
+            st.video('https://www.youtube.com/watch?v=jEy_czb3RKA')
+
+        with channel2:
+            st.subheader('Jeff Nippard')
+            st.text('Powerlifter/Trainer')
+            st.video('https://www.youtube.com/watch?v=ptpmRrzRtWQ')
+
+        with channel3:
+            st.subheader('Russel Orhii')
+            st.text('Champion Powerlifter')
+            st.video('https://www.youtube.com/watch?v=JNSJZKyc_Lw')
