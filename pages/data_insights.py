fdf8bfdb02e007501374a25a267a6bf03b00f4d0
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-28 23:12:34 -0500

    Adding labels to data insights page

--- pages/data_insights.py
+++ pages/data_insights.py
@@ -1,7 +1,9 @@
-import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
+
 from pages import utils
+
 
 def app():
     st.subheader('Data Visualizations')
@@ -11,3 +13,5 @@ def app():
     plt.figure(figsize=(9, 6))
     fig = sns.countplot(x='AgeClass', data=data).figure
     st.pyplot(fig)
+
+    st.text('Informational figure')
