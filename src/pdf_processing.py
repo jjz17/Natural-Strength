f35dec1723ca6d18cd3fabe49a8f42705e15465f
Jason Zhang <jasonjzhang17@gmail.com>
2022-02-22 22:26:22 -0500

    Created PDF processing file

--- src/pdf_processing.py
+++ src/pdf_processing.py
@@ -1,1 +1,10 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def extract_pdf_text(file_path: str):
+    pdf = PdfFileReader(file_path)
+
+    file_name = file_path.split('.')[0]
+    with open(f'{file_name}.txt', 'w') as f:
+        for page_num in range(pdf.numPages):
+            print(f'Page: {page_num}')
+            page_obj = pdf.getPage(page_num)
