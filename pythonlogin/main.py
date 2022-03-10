f8138974e955040814e07d3fb2e123b7357863b2
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-09 22:15:07 -0500

    Creating profile update page

--- pythonlogin/main.py
+++ pythonlogin/main.py
@@ -19,6 +19,8 @@ mysql = MySQL(app)
 
 # http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
 # @app.route('/pythonlogin/', methods=['GET', 'POST'])
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
@@ -30,7 +32,8 @@ def login():
         password = request.form['password']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
         # If account exists in accounts table in out database
@@ -49,17 +52,21 @@ def login():
 
 # http://localhost:5000/pythonlogin/logout - this will be the logout page
 # @app.route('/pythonlogin/logout')
+
+
 @app.route('/logout')
 def logout():
     # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
 
 # http://localhost:5000/pythonlogin/register - this will be the registration page, we need to use both GET and POST requests
 # @app.route('/pythonlogin/register', methods=['GET', 'POST'])
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
@@ -72,7 +79,8 @@ def register():
         email = request.form['email']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s', (username,))
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
@@ -85,7 +93,8 @@ def register():
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute(
+                'INSERT INTO accounts (`username`, `password`, `email`) VALUES (%s, %s, %s)', (username, password, email,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
@@ -95,6 +104,8 @@ def register():
     return render_template('register.html', msg=msg)
 
 # http://localhost:5000/pythonlogin/home - this will be the home page, only accessible for loggedin users
+
+
 @app.route('/home')
 def home():
     # Check if user is loggedin
@@ -105,13 +116,16 @@ def home():
     return redirect(url_for('login'))
 
 # http://localhost:5000/pythonlogin/profile - this will be the profile page, only accessible for loggedin users
+
+
 @app.route('/profile')
 def profile():
     # Check if user is loggedin
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        cursor.execute('SELECT * FROM accounts WHERE id = %s',
+                       (session['id'],))
         account = cursor.fetchone()
         # Show the profile page with account info
         return render_template('profile.html', account=account)
@@ -119,6 +133,8 @@ def profile():
     return redirect(url_for('login'))
 
 # http://localhost:5000/pythonlogin/profile - this will be the profile edit page, only accessible for loggedin users
+
+
 @app.route('/edit')
 def edit():
     # Check if user is loggedin
@@ -126,6 +142,73 @@ def edit():
         # Allow user to edit information and send it to the MySQL Database
         pass
     pass
+
+
+@app.route('/update')
+def update():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        id = session['id']
+        # We need all the account info for the user so we can display it on the profile page
+        # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        # cursor.execute('SELECT * FROM accounts WHERE id = %s',
+        #                (session['id']))
+        # account = cursor.fetchone()
+
+        # Output message if something goes wrong...
+        msg = ''
+        account = None
+        # Check if "username", "password" and "email" POST requests exist (user submitted form)
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+            # Create variables for easy access
+            username = request.form['username']
+            password = request.form['password']
+            age = request.form['age']
+            weight = request.form['weight']
+            squat = request.form['squat']
+            bench = request.form['bench']
+            deadlift = request.form['deadlift']
+            email = request.form['email']
+
+            # # Check if account exists using MySQL
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+            account = cursor.fetchone()
+
+            # Validation checks
+            if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address!'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers!'
+            elif not username or not password or not email:
+                msg = 'Please fill out the form!'
+            elif not re.match(r'^\+?(0|[1-9]\d*)$', age):
+                msg = 'Age must be an int'
+            elif not re.match(r'^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$', weight):
+                msg = 'Weight must be a positive number'
+            elif not re.match(r'^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$', squat):
+                msg = 'Squat must be a positive number'
+            elif not re.match(r'^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$', bench):
+                msg = 'Bench must be a positive number'
+            elif not re.match(r'^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$', deadlift):
+                msg = 'Deadlift must be a positive number'
+            else:
+                # Account doesnt exists and the form data is valid, now update data into accounts table
+                cursor.execute(
+                    f'UPDATE accounts SET username = {username}, password = {password}, age = {age}, weight = {weight}, squat = {squat}, bench = {bench}, deadlift = {deadlift}, email = {email} WHERE id = {id}')
+                mysql.connection.commit()
+                msg = 'You have successfully updated!'
+        elif request.method == 'POST':
+            # Form is empty... (no POST data)
+            msg = 'Please fill out the form!'
+
+        # Show registration form with message (if any)
+        # return render_template('register.html', msg=msg)
+
+        # Show the profile page with account info
+        return render_template('update.html', account=account, msg=msg)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
