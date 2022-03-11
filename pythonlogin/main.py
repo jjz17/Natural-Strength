aa037c0d86d4d543976517488f613f105ddad14b
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-11 10:37:51 -0500

    Added dynamically updating plot to flask app

--- pythonlogin/main.py
+++ pythonlogin/main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import io
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 
@@ -202,6 +206,44 @@ def update():
         return render_template('update.html', msg=msg)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+
+@app.route('/<int:points>', methods=['GET'])
+def plot(points):
+    title = 'Plot'
+    plot = plot_points(points)
+    return render_template('plot.html', title=title, plot=plot)
+
+
+def plot_points(points):
+    """Generate a plot with a varying number of randomly generated points
+    Args:
+    points (int): a number of points to plot
+    Returns: An svg plot with <points> data points
+    """
+    # data for plotting
+    data = np.random
+
+    data = np.random.rand(points, 2)
+
+    fig = Figure()
+    FigureCanvas(fig)
+
+    ax = fig.add_subplot(111)
+
+    ax.scatter(data[:,0], data[:,1])
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title(f'There are {points} data points!')
+    ax.grid(True)
+
+    img = io.StringIO()
+    fig.savefig(img, format='svg')
+    #clip off the xml headers from the image
+    svg_img = '<svg' + img.getvalue().split('<svg')[1]
+    
+    return svg_img
 
 
 if __name__ == '__main__':
