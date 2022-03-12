14961fbec910492c4fc240f18a28fef8b6112143
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-12 10:16:33 -0500

    Sqlalchemy orm functioning

---
+++ website/inserts.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+# 1 - imports
+from datetime import date
+
+from user import User
+from base import Session, engine, Base
+from user_metrics import UserMetrics
+
+# 2 - generate database schema
+Base.metadata.create_all(engine)
+
+# 3 - create a new session
+session = Session()
+
+# 4 - create users
+jz = User('JZ', 'a', date(2002, 7, 19), 'j@j.j')
+
+# 5 - add user metrics to users
+jz_met1 = UserMetrics(jz, 5, 5, 5, 5)
+
+# 6 - persist data
+session.add(jz)
+
+session.add(jz_met1)
+
+# 7 - commit and close session
+session.commit()
+session.close()
---
+++ website/user_metrics.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from sqlalchemy import Column, Integer, Float, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from base import Base
+
+
+class UserMetrics(Base):
+    __tablename__ = 'user_metrics'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", backref="user_metrics")
+    weight = Column(Float)
+    squat = Column(Float)
+    bench = Column(Float)
+    deadlift = Column(Float)
+    date = Column(Date)
+
+    def __init__(self, user, weight, squat, bench, deadlift):
+        self.user = user
+        self.weight = weight
+        self.squat = squat
+        self.bench = bench
+        self.deadlift = deadlift
