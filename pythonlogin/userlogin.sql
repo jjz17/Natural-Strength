e4752b2d6a8a963e728f13acd2643733288a8ab7
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-12 10:05:04 -0500

    Refactored users table

--- pythonlogin/userlogin.sql
+++ pythonlogin/userlogin.sql
@@ -2,26 +2,22 @@ CREATE DATABASE IF NOT EXISTS `pythonlogin` DEFAULT CHARACTER SET utf8 COLLATE u
 USE `pythonlogin`;
 
 SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `users`;
 SET foreign_key_checks = 1;
-CREATE TABLE IF NOT EXISTS `accounts` (
+CREATE TABLE IF NOT EXISTS `users` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
   	`username` varchar(50) NOT NULL,
   	`password` varchar(255) NOT NULL,
 	`birth_date` DATE,
   	`email` varchar(100) NOT NULL,
-	`weight` FLOAT,
-	`squat` FLOAT,
-	`bench` FLOAT,
-	`deadlift` FLOAT,
     PRIMARY KEY (`id`),
     CONSTRAINT `uc_user` UNIQUE (`id`, `username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
-INSERT INTO `accounts` (`username`, `password`, `email`) VALUES ('jason', 'password', 'jasonjzhang17@gmail.com');
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+INSERT INTO `users` (`username`, `password`, `email`) VALUES ('jason', 'password', 'jasonjzhang17@gmail.com');
 
-CREATE TABLE IF NOT EXISTS `weights` (
+CREATE TABLE IF NOT EXISTS `user_metrics` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
   	`user_id` int(11) NOT NULL,
 	`weight` FLOAT,
@@ -32,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `weights` (
     PRIMARY KEY (`id`),
     CONSTRAINT fk_user
     FOREIGN KEY (`user_id`) 
-    REFERENCES accounts(`id`)
+    REFERENCES users(`id`)
         ON UPDATE CASCADE
         ON DELETE CASCADE  
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
