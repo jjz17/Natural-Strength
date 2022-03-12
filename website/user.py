4cdacfaf86f563c4f5778e2d684cf0b50e1751f6
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-12 10:09:28 -0500

    Refactored user_metrics table

--- website/user.py
+++ website/user.py
@@ -1,6 +1,6 @@
 
 from dataclasses import dataclass
-from sqlalchemy import Column, String, Integer, Date, Float
+from sqlalchemy import Column, String, Integer, Date
 from base import Base
 
 class User(Base):
@@ -10,18 +10,10 @@ class User(Base):
     username = Column(String)
     password = Column(String)
     birth_date = Column(Date)
-    weight = Column(Float)
-    squat = Column(Float)
-    bench = Column(Float)
-    deadlift = Column(Float)
     email = Column(String)
 
-    def __init__(self, username, password, birth_date, email, weight=0, squat=0, bench=0, deadlift=0):
+    def __init__(self, username, password, birth_date, email):
         self.username = username
         self.password = password
         self.birth_date = birth_date
         self.email = email
-        self.weight = weight
-        self.squat = squat
-        self.bench = bench
-        self.deadlift = deadlift
