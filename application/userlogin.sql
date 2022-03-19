7426057262e6000f2fadc1eb19a98b76bfe8e57c
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-19 09:42:23 -0400

    Implemented radio buttons for registering sex for user

--- application/userlogin.sql
+++ application/userlogin.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
   	`username` varchar(50) NOT NULL,
   	`password` varchar(255) NOT NULL,
+	`sex` TINYINT(1) NOT NULL,
 	`birth_date` DATE,
   	`email` varchar(100) NOT NULL,
     PRIMARY KEY (`id`),
