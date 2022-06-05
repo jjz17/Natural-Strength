1ba09346637ed915658bf1ae0f94d114e796102d
Jason Zhang <jasonjzhang17@gmail.com>
2022-06-05 19:04:02 -0400

    Implementing email send function

--- application/utils.py
+++ application/utils.py
@@ -1,9 +1,53 @@
 import numpy as np
+import smtplib, ssl
+## email.mime subclasses
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+## The pandas library is only for generating the current date, which is not necessary for sending emails
+import pandas as pd
 
 import _pickle as cPickle
 
 from application.user_metrics import UserMetrics, DummyUserMetrics
 
+def send_notif_emails(users: list):
+    # Set up the email addresses and password. Please replace below with your email address and password
+    email_from = 'a.plus.or.no.rice@gmail.com'
+    password = 'jiajia2002'
+
+    for user in users:
+        # If User has notifications turned on
+        if user.notifications == 1:
+            email_to = user.email
+
+            # Define the HTML document
+            html = f'''
+                <html>
+                    <body>
+                        <h1>Daily S&P 500 prices report</h1>
+                        <p>Hello {user.username}, welcome to your report!</p>
+                    </body>
+                </html>
+                '''
+            # Generate today's date to be included in the email Subject
+            date_str = pd.Timestamp.today().strftime('%Y-%m-%d')
+
+            # Create a MIMEMultipart class, and set up the From, To, Subject fields
+            email_message = MIMEMultipart()
+            email_message['From'] = email_from
+            email_message['To'] = email_to
+            email_message['Subject'] = f'Report email - {date_str}'
+
+            # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
+            email_message.attach(MIMEText(html, "html"))
+            # Convert it as a string
+            email_string = email_message.as_string()
+
+            # Connect to the Gmail SMTP server and Send Email
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                server.login(email_from, password)
+                server.sendmail(email_from, email_to, email_string)
 
 def choose_pred(current, pred):
     result = max(current, pred)
